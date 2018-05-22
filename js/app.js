98876c72b4d7d9c0963495d489db12320584cb40
Juan Jose Capellan <soycape@hotmail.com>
2018-05-22 14:41:09 +0200

    Display information

--- js/app.js
+++ js/app.js
@@ -9,13 +9,14 @@ window.onload = function () {
 
 var aimingtest = {};
 
-/** Load state ****/
+/**** Load state ****/
 
 aimingtest.boot = function (game) {};
 
 aimingtest.boot.prototype = function (game) {};
 
 aimingtest.boot.prototype = {
+    
     preload: function () {
 
         this.load.image('target', 'assets/images/target.png');
@@ -38,7 +39,7 @@ aimingtest.boot.prototype = {
 
 
 
-/** Menu state ****/
+/**** Menu state ****/
 
 aimingtest.menu = function (game) {};
 
@@ -49,6 +50,7 @@ aimingtest.menu.prototype = {
     create: function () {
 
         this.stage.backgroundColor = 'rgb(106,98,60)';
+        document.getElementById('display').innerHTML='AIMING TEST V1.0';
         var text;
         var textStyle = {
             font: "bold 32px Arial",
@@ -81,7 +83,7 @@ aimingtest.menu.prototype = {
 }
 
 
-/** InGame state ****/
+/**** InGame state ****/
 
 aimingtest.inGame = function (game) {};
 
@@ -92,6 +94,12 @@ aimingtest.inGame.prototype = {
         // Arrays initialization
         this.shotsArray = [];
         this.hitsArray = [];
+        //Best score initialization
+        var cookie = this.getCookie('best');
+        this.bestScore = (cookie != '') ? parseInt(cookie) : 0;
+        // Display html element
+        this.display = document.getElementById('display');
+        this.display.innerHTML = '10 targets left'
         // Headers hitsArray
         this.hitsArray.push(['HIT', 'TIME', 'DISTANCE', 'SCORE']);
         // True when reloading
@@ -102,8 +110,8 @@ aimingtest.inGame.prototype = {
         this.target;
         // Hits counter
         this.hitsCounter = 0;
-        // Shots counter
-        this.shotsCounter = 0;
+        // Targets counter
+        this.targetsCounter = 0;
         //Failed shots counter
         this.failedShotsCounter = 0;
         // Bonus distance counter
@@ -116,7 +124,11 @@ aimingtest.inGame.prototype = {
         this.pointsDistanceBonus = 400;
         // Target spawn time stamp
         this.targetStartTime;
+        // Base score
         this.score = 0;
+        // Base score + bonus
+        this.totalScore = 0;
+
         var t = this;
 
         // Particles emitter
@@ -132,7 +144,7 @@ aimingtest.inGame.prototype = {
         this.emitter.lifespan = 1000;
         this.emitter.height = 90;
         this.emitter.width = 90;
-        console.log(this.emitter);
+        this.emitter.maxParticles=30;
 
         //Sound effects
         this.sndShot = this.add.audio('shot', 1, false);
@@ -180,7 +192,6 @@ aimingtest.inGame.prototype = {
                     t.hitsCounter++;
                     // Time elapsed until impact.
                     var timeSpan = parseInt(targetHitTime - t.targetStartTime);
-                    console.log(timeSpan);
                     // Hit score
                     var hitScore = parseInt(400 - distance * 2 - timeSpan / 10);
                     // Total score
@@ -194,16 +205,14 @@ aimingtest.inGame.prototype = {
                     };
                     // New hit in the hits array
                     t.hitsArray.push([t.hitsCounter, timeSpan + ' ms', distance + ' px', hitScore + ' pts']);
-                    /*// Updating the score in the UI
-                    document.getElementById('score').innerHTML=t.score;*/
                     // Reset target in new position
                     t.clearTimer();
                     t.target.visible = false;
-                    if (this.shotsCounter == 10) {
-                        this.showResults();
+                    if (this.targetsCounter == 10) {
+                        this.gameOver();
                         return;
                     };
-                    setTimeout(t.resetTarget.bind(t), this.math.between(800, 1500));
+                    setTimeout(t.resetTarget.bind(t), this.math.between(300, 1000));
 
                 } else {
                     t.failedShotsCounter++;
@@ -223,19 +232,20 @@ aimingtest.inGame.prototype = {
     },
 
     resetTarget: function () {
-        if (this.shotsCounter > 10) {
-            this.showResults();
+        if (this.targetsCounter == 10) {
+            this.gameOver();
             return;
         };
-        this.shotsCounter++;
+        this.targetsCounter++;
         this.sndTarget.play();
         this.target.reset(this.math.between(80, 960 - 80), this.math.between(80, 540 - 80));
         this.targetStartTime = performance.now();
         this.setTimer();
+        this.display.innerHTML = (10 - this.targetsCounter) + ' targets left';
     },
 
     setTimer: function () {
-        this.targetTimer = setTimeout(this.resetTarget.bind(this), 1000);
+        this.targetTimer = setTimeout(this.resetTarget.bind(this), this.math.between(800, 1000));
     },
 
     clearTimer: function () {
@@ -249,6 +259,27 @@ aimingtest.inGame.prototype = {
         var distance = Math.sqrt(dx * dx + dy * dy);
         return distance;
 
+    },
+
+    gameOver: function () {
+
+        this.setTotalScore();
+
+        // Check new record
+        if (this.bestScore < this.totalScore) {
+            this.display.innerHTML = 'You got a new record !!!';
+            this.setCookie('best',this.totalScore,180);
+
+        } else {
+            this.display.innerHTML = 'Your best score is ' + this.bestScore.toString();
+        };
+
+        this.showResults();
+
+    },
+
+    setTotalScore: function () {
+        this.totalScore = this.score + this.bonusDistanceCounter * this.pointsDistanceBonus + this.bonusTimeCounter * this.pointsTimeBonus - this.failedShotsCounter * 400;        
     },
 
     showResults: function () {
@@ -303,8 +334,7 @@ aimingtest.inGame.prototype = {
             tableResults.appendChild(row);
         });
 
-        // Total score
-        var totalScore = this.score + this.bonusDistanceCounter * this.pointsDistanceBonus + this.bonusTimeCounter * this.pointsTimeBonus - this.failedShotsCounter * 400;
+        // Total score        
         var row = document.createElement('tr');
         var cell = document.createElement('th');
         cell.setAttribute('colspan', 4);
@@ -318,13 +348,15 @@ aimingtest.inGame.prototype = {
         cell.style.backgroundColor = 'red';
         cell.style.color = 'white';
         cell.style.fontSize = '1.5em';
-        cell.textContent = totalScore.toString();
+        cell.textContent = this.totalScore.toString();
         row.appendChild(cell);
         tableResults.appendChild(row);
         tableResults.style.opacity = '1';
 
+
         document.getElementById('replay').innerHTML = 'Click to replay';
 
+        // Changes onDown event
         this.input.onDown.removeAll();
         this.input.onDown.add(this.replay, this);
 
@@ -336,8 +368,31 @@ aimingtest.inGame.prototype = {
         tableResults.style.opacity = 0;
         tableResults.innerHTML = '';
         document.getElementById('replay').innerHTML = '';
-
+        // starts menu state
         this.state.start('menu');
+    },
+
+    setCookie: function (c_name, c_value, c_days) {
+        var d = new Date();
+        d.setTime(d.getTime() + (c_days * 24 * 60 * 60 * 1000));
+        var expires = "expires=" + d.toUTCString();
+        document.cookie = c_name + "=" + c_value + ";" + expires + ";path=/";
+    },
+
+    getCookie: function (c_name) {
+        var name = c_name + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
     }
 
 
